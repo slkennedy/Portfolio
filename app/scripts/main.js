c9475bde3aef41ea2eb713b76c0c34936afe7b70
slkennedy <slkennedy@gmail.com>
2014-11-04 11:56:55 -0500

    Adds smooth scrolling to site sections

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,21 @@
 console.log('If you like what you see in my portfolio, contact me at slkennedy@gmail.com');
+
+$('#about-link').click(function() {
+	$('html, body').animate ({ scrollTop:0}, 200);
+	return false;
+});
+
+$('#skills-link').click(function() {
+	$('html, body').animate ({ scrollTop:420}, 200);
+	return false;
+});
+
+$('#works-link').click(function() {
+	$('html, body').animate ({ scrollTop:709}, 200);
+	return false;
+});
+
+$('#contact-link').click(function() {
+	$('html, body').animate ({ scrollTop:2000}, 200);
+	return false;
+});
