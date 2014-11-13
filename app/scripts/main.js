fa48798d75dabfa4df95e02a39066c01fe7b67ce
slkennedy <slkennedy@gmail.com>
2014-11-13 01:44:34 -0500

    completes main styling and adds images and links to featured work

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,6 @@
 console.log('If you like what you see in my portfolio, contact me at slkennedy@gmail.com');
 
+//Menu scroll script
 $('#about-link').click(function() {
 	$('html, body').animate ({ scrollTop:0}, 200);
 	return false;
@@ -11,11 +12,11 @@ $('#skills-link').click(function() {
 });
 
 $('#works-link').click(function() {
-	$('html, body').animate ({ scrollTop:709}, 200);
+	$('html, body').animate ({ scrollTop:710}, 200);
 	return false;
 });
 
 $('#contact-link').click(function() {
-	$('html, body').animate ({ scrollTop:2000}, 200);
+	$('html, body').animate ({ scrollTop:2056}, 200);
 	return false;
 });
