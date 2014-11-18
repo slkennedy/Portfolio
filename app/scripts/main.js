a9645495dc8f93a42132022c68d17978b9c7f51e
slkennedy <slkennedy@gmail.com>
2014-11-17 19:13:55 -0500

    Adds resume and wordpress links. Builds Resume HTML

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,22 +2,22 @@ console.log('If you like what you see in my portfolio, contact me at slkennedy@g
 
 //Menu scroll script
 $('#about-link').click(function() {
-	$('html, body').animate ({ scrollTop:0}, 200);
+	$('html, body').animate ({ scrollTop:0}, 350);
 	return false;
 });
 
 $('#skills-link').click(function() {
-	$('html, body').animate ({ scrollTop:370}, 200);
+	$('html, body').animate ({ scrollTop:370}, 350);
 	return false;
 });
 
 $('#works-link').click(function() {
-	$('html, body').animate ({ scrollTop:653}, 200);
+	$('html, body').animate ({ scrollTop:653}, 350);
 	return false;
 });
 
 $('#contact-link').click(function() {
-	$('html, body').animate ({ scrollTop:1878}, 200);
+	$('html, body').animate ({ scrollTop:1878}, 350);
 	return false;
 });
 
